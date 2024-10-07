9e91640b39ad0403ad396e6207f6486b3ec07a3c
RICARDAOSCOTT <163008691+RICARDAOSCOTT@users.noreply.github.com>
2024-10-07 11:32:49 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+CriaCartao (
+    'Programação 2.0',
+    'O que é phyton',
+    'O phyton é uma linguagem de programação'
+)
+
+CriaCartao (
+    'Rei do futebol',
+    'Quem é considerado o Rei do Futebol',
+    'Pelé'
+)
+
+CriaCartao (
+    'Maior campeão',
+    'Qual é o maior campeão da Champions League',
+    'Real Madrid'
+)
+
+CriaCartao (
+    'Maior População',
+    'Quais são os países com maior população do planeta',
+    'India e China'
+)
+
+CriaCartao (
+    'Violência na 2WW',
+    'Qual foi o dia mais sangrento da 2WW',
+    'Dia D'
+)
+
+CriaCartao (
+    'Rei do Pop',
+    'Quem é considerado o Rei do Pop',
+    'Michael Jackson'
+)
+
+CriaCartao (
+    'Touro na Logo',
+    'Qual a marca de carro que possui um touro na logo',
+    'Lamborghini'
+)
